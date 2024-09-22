c5d363441e2be685f65c92a8b72350ae0048f626
septiannaim <naimplung28@gmail.com>
2024-09-22 23:49:02 +0700

    first commit

---
+++ data/index.ts
@@ -0,0 +1,212 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/kampung.webp",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Terletak dekat Kota Malang namun tetap menjaga kearifan lokal",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Menjaga Tradisi Tetap Hidup dalam Langkah Maju.",
+    description: "Terus Berinovasi dan Beradaptasi,",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Pecinta Budaya dengan Semangat Melestarikan Tradisi",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/batik.png",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title:
+      "Pekan Budaya dan Pasar Rakyat, wadah kreatifitas dan kebersamaan warga",
+    description: "Menyemarakkan Budaya Setiap Tahun",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b1.jpg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Bersama Melestarikan Seni dan Budaya Kampung Cempluk",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Jaran Kepang (Kuda Lumping)",
+    des: "Pertunjukan yang menggabungkan gerakan tari, musik, dan tradisi magis. Jaran Kepang menjadi wujud nyata dari semangat gotong royong dan kegembiraan warga Kampung Cempluk.",
+    img: "/b2.jpg",
+    iconLists: ["/logo.png"],
+    link: "",
+  },
+  {
+    id: 2,
+    title: "Ande Ande Lumut",
+    des: "Sebuah kesenian yang langka, Ande-Ande Lumut menceritakan kisah rakyat dengan gaya yang unik. Seni ini menjadi salah satu daya tarik utama di Kampung Cempluk dan bagian penting dari festival budaya yang diadakan.",
+    img: "/b3.jpg",
+    iconLists: ["/logo.png"],
+    link: "",
+  },
+  {
+    id: 3,
+    title: "Tari Tradisional Nusantara",
+    des: "Para penari dari Kampung Cempluk menyalurkan cerita rakyat melalui gerakan yang anggun dan penuh makna. Setiap gerakan adalah penghormatan terhadap tradisi yang diwariskan dari generasi ke generasi.",
+    img: "/b4.jpg",
+    iconLists: ["logo.png"],
+    link: "",
+  },
+  {
+    id: 4,
+    title: "Barong Singo Yudo",
+    des: "Menampilkan pertunjukan seni Barong Singo Yudho, yang merupakan simbol keberanian dan kekuatan masyarakat Kampung Cempluk. Pertunjukan ini tidak hanya memukau, tetapi juga melestarikan warisan budaya lokal.",
+    img: "/b5.jpg",
+    iconLists: ["logo.png"],
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Kampung Cempluk bukan sekedar tempat, tapi sebuah ruang hidup di mana tradisi dan budaya bernafas dalam setiap gerakan seni yang ditampilkan warganya.",
+    name: "Priyo Sidhi",
+    title: "Penggagas Festival Kampung Cempluk",
+  },
+  {
+    quote:
+      "Di tengah modernitas yang kian menggerus, Kampung Cempluk hadir sebagai penjaga warisan, menghidupkan kembali seni-seni tradisional yang hampir terlupakan",
+    name: "Redy Eko Prastyo",
+    title: "Budayawan dan Penggerak Kesenian Kampung Cempluk",
+  },
+  {
+    quote:
+      "Setiap sudut Kampung Cempluk adalah panggung budaya yang menceritakan kisah tentang kegigihan, kebersamaan, dan kecintaan terhadap tradisi.",
+    name: "Bapak Sulaiman",
+    title: "Sesepuh dan Tokoh Masyarakat Kampung Cempluk",
+  },
+  {
+    quote:
+      "Kampung Cempluk adalah bukti nyata bahwa kesenian rakyat tidak akan pernah mati selama ada jiwa-jiwa yang mencintainya. Di sini, budaya bukan hanya dilestarikan, tapi juga dirayakan.",
+    name: "Denny Mizar",
+    title: "Anggota Komunitas Pelangi Sastra Malang",
+  },
+  {
+    quote:
+      "Di Kampung Cempluk, kita tidak hanya menyaksikan pertunjukan seni, tetapi juga merasakan denyut kehidupan dan semangat warga yang tiada henti menjaga identitas mereka.",
+    name: "Bapak Sukadi",
+    title: "Tokoh Seni Tradisional Kampung Cempluk",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "Sanggar Seni dan Budaya",
+    img: "/6.png",
+    nameImg: "/Sanggar Seni.svg",
+  },
+  {
+    id: 2,
+    name: "Komunitas Budayawan",
+    img: "/7.png",
+    nameImg: "/Komunitas.svg",
+  },
+  {
+    id: 3,
+    name: "Sponsor",
+    img: "/8.png",
+    nameImg: "/Sponsor.svg",
+  },
+  {
+    id: 4,
+    name: "Mitra Kesenian Nusantara",
+    img: "/9.png",
+    nameImg: "/Kesenian Nusantara.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Menghidupkan Seni dan Budaya Lokal",
+    desc: "Melibatkan warga dalam berbagai kegiatan kesenian seperti Barong Singo Yudho, Jaran Kepang, dan Pencak Silat untuk melestarikan tradisi yang hampir punah.",
+    className: "md:col-span-2",
+    thumbnail: "/1.png",
+  },
+  {
+    id: 2,
+    title: "Menggali Nilai Tradisi, Membangun Identitas Komunitas",
+    desc: "Menjaga dan mengembangkan kesenian tradisional melalui pelatihan, festival tahunan, dan pertunjukan seni yang melibatkan berbagai kalangan, dari anak-anak hingga orang tua.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/2.png",
+  },
+  {
+    id: 3,
+    title: "Ruang Apresiasi Seni bagi Komunitas",
+    desc: "Menyelenggarakan Festival Kampung Cempluk sebagai wadah bagi seniman lokal untuk menampilkan karya mereka, mempertemukan berbagai komunitas kesenian dalam semangat kebersamaan.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/3.png",
+  },
+  {
+    id: 4,
+    title: "Berbagi dan Belajar dari Komunitas Lain",
+    desc: "Melakukan studi banding dan kolaborasi dengan sanggar-sanggar seni di berbagai daerah untuk memperkaya pengalaman dan meningkatkan kebersamaan kesenian di Kampung Cempluk.",
+    className: "md:col-span-2",
+    thumbnail: "/4.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
